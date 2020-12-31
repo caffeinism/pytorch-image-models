dd77687a3e51b6f2fb582fe6b3f6f8a3603e5dff
Minjong Kim <make.dirty.code@gmail.com>
2020-12-31 11:56:37 +0900

    Update ensemble.py to use calibrated thresholds

--- ensemble.py
+++ ensemble.py
@@ -1,29 +1,34 @@
 import numpy as np
 
-def ensemble(logit_lists, targets=None, thresholds=None):
-    logits = sum(logit_lists) / len(logit_lists)
-
+def ensemble(pred_lists, targets=None, thresholds=None):    
     if thresholds is not None:
         print('use custom threshold')
         thresholds = thresholds
     elif targets is not None:
         print('do calibration...')
-        start, end, period = 5, 100, 5
-        f1s = []
-        for i in range(5, 100, 5):
-            threshold = i / 100
-            from sklearn.metrics import f1_score, precision_score, recall_score
-            f1 = f1_score(targets, logits > threshold, average=None)
-            f1s.append(f1)
-            print('threshold: {}\n  F1: {}'.format(threshold, f1))
-
-        thresholds = (start + np.argmax(np.array(f1s), axis=0) * period) * 0.01
+        thresholds = calibrate(pred_lists, targets)
         print('custom_thresholds: ', thresholds)
     else:
         print('use default threshold')
         thresholds = 0.5
+        
+    preds = sum(pred_lists) / len(pred_lists)
+    prediction = preds > thresholds
+    
+    return preds, prediction
 
-    prediction = logits > thresholds
+def calibrate(pred_lists, targets):
+    preds = sum(pred_lists) / len(pred_lists)
+    
+    start, end, period = 5, 100, 5
+    f1s = []
+    for i in range(5, 100, 5):
+        threshold = i / 100
+        from sklearn.metrics import f1_score, precision_score, recall_score
+        f1 = f1_score(targets, preds > threshold, average=None)
+        f1s.append(f1)
+#         print('threshold: {}\n  F1: {}'.format(threshold, f1))
 
-    return logits, prediction
+    thresholds = (start + np.argmax(np.array(f1s), axis=0) * period) * 0.01
 
+    return thresholds
