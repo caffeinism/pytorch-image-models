74e0ff7168497f4179fff8f9f8f24a0fe17f06c8
Minjong Kim <make.dirty.code@gmail.com>
2020-12-31 15:15:28 +0900

    Update validate.py
    +validate with tta
    +save result with iitp format

--- validate.py
+++ validate.py
@@ -17,9 +17,10 @@ import torch
 import torch.nn as nn
 import torch.nn.parallel
 import numpy as np
+import json
 from collections import OrderedDict
 from contextlib import suppress
-from ensemble import ensemble, calibrate
+from ensemble import calibrate
 
 from timm.models import create_model, apply_test_time_pool, load_checkpoint, is_model, list_models
 from timm.data import MultiLabelDataset, CocoDataset, Dataset, DatasetTar, create_loader, resolve_data_config, RealLabelsImagenet
@@ -134,26 +135,7 @@ def load_checkpoint(model, checkpoint, use_ema=False, strict=True):
     state_dict = load_state_dict(checkpoint, use_ema)
     model.load_state_dict(state_dict, strict=strict)
     
-    
-def validate(args, checkpoint=None):
-    # might as well try to validate something
-    args.pretrained = args.pretrained or not checkpoint
-    args.prefetcher = not args.no_prefetcher
-    amp_autocast = suppress  # do nothing
-    if args.amp:
-        if has_apex:
-            args.apex_amp = True
-        elif has_native_amp:
-            args.native_amp = True
-        else:
-            _logger.warning("Neither APEX or Native Torch AMP is available, using FP32.")
-    assert not args.apex_amp or not args.native_amp, "Only one AMP mode should be set."
-    if args.native_amp:
-        amp_autocast = torch.cuda.amp.autocast
-
-    if args.legacy_jit:
-        set_jit_legacy()
-
+def get_model(args, checkpoint):
     # create model
     model = create_model(
         args.model,
@@ -186,7 +168,46 @@ def validate(args, checkpoint=None):
 
     if args.num_gpu > 1:
         model = torch.nn.DataParallel(model, device_ids=list(range(args.num_gpu)))
+    model.eval()
+    
+    return model, data_config
+    
+def validate(args, checkpoints=list()):
+    # might as well try to validate something
+    args.pretrained = False
+    args.prefetcher = not args.no_prefetcher
+    amp_autocast = suppress  # do nothing
+    if args.amp:
+        if has_apex:
+            args.apex_amp = True
+        elif has_native_amp:
+            args.native_amp = True
+        else:
+            _logger.warning("Neither APEX or Native Torch AMP is available, using FP32.")
+    assert not args.apex_amp or not args.native_amp, "Only one AMP mode should be set."
+    if args.native_amp:
+        amp_autocast = torch.cuda.amp.autocast
 
+    if args.legacy_jit:
+        set_jit_legacy()
+        
+    
+    models = []
+    for checkpoint in checkpoints:
+        data = torch.load(checkpoint, map_location='cpu')
+
+        args.model = data['args'].model
+        args.amp   = data['args'].amp
+        args.num_classes = data['args'].num_classes
+        args.use_ema = data['args'].model_ema
+
+        model, data_config = get_model(args, data)
+        models.append(model)
+
+    args.dataset_type = data['args'].dataset_type
+    args.use_multi_label = data['args'].use_multi_label
+
+        
     if args.use_multi_label:
         criterion = nn.BCEWithLogitsLoss().cuda()
     else:
@@ -199,7 +220,7 @@ def validate(args, checkpoint=None):
     elif args.dataset_type == 'coco':
         dataset = CocoDataset(args.data)
     elif args.dataset_type == 'multi':
-        dataset = MultiLabelDataset(args.data)
+        dataset = MultiLabelDataset(args.data, feed_filename=True)
     else:
         raise NotImplemented
 
@@ -218,6 +239,8 @@ def validate(args, checkpoint=None):
 #     crop_pct = 1.0 if test_time_pool else data_config['crop_pct']
     print('crop_pct:', data_config['crop_pct'])
     crop_pct = data_config['crop_pct']
+    
+    data_config['input_size'] = (720, 1280)
     loader = create_loader(
         dataset,
         input_size=data_config['input_size'],
@@ -227,32 +250,57 @@ def validate(args, checkpoint=None):
         mean=data_config['mean'],
         std=data_config['std'],
         num_workers=args.workers,
-        crop_pct=crop_pct,
+        crop_pct=1.0, # crop_pct,
         pin_memory=args.pin_mem,
         tf_preprocessing=args.tf_preprocessing)
 
     batch_time = AverageMeter()
 
-    model.eval()
+    import ttach as tta
+    
+    tta_model = tta.ClassificationTTAWrapper(
+        models=[torch.nn.Sequential(
+            model,
+            torch.nn.Sigmoid(),
+        ) for model in models],
+        transforms=tta.Compose(
+            [
+                tta.FiveCrops(args.img_size, args.img_size),
+                tta.HorizontalFlip(),
+#                 tta.Rotate90(angles=[0, 90, 180, 270]),
+#                 tta.Scale(scales=[1, 2, 4]),
+#                 tta.Multiply(factors=[0.9, 1, 1.1]),        
+            ]
+        ),
+        merge_mode='max',
+    )
+    
     with torch.no_grad():
         # warmup, reduce variability of first batch time, especially for comparing torchscript vs non
-        input = torch.randn((args.batch_size,) + data_config['input_size']).cuda()
+        input = torch.randn((args.batch_size, 3) + data_config['input_size']).cuda()
         if args.channels_last:
             input = input.contiguous(memory_format=torch.channels_last)
         model(input)
         
-        preds, targets = [], []
+        preds, targets, filenames = [], [], []
         end = time.time()
-        for batch_idx, (input, target) in enumerate(loader):
+        for batch_idx, ((input, target), filename) in enumerate(loader):
             if args.no_prefetcher:
                 target = target.cuda()
                 input = input.cuda()
             if args.channels_last:
                 input = input.contiguous(memory_format=torch.channels_last)
 
+#             input = input[:, :, :, 280:280+720]
+            
+#             from IPython.display import display
+#             from PIL import Image
+#             display(Image.fromarray(((input[0].cpu().permute(1, 2, 0)*torch.FloatTensor(data_config['std'])+torch.FloatTensor(data_config['mean']))*255).byte().numpy()))
+            
             # compute output
             with amp_autocast():
-                output = model(input)
+#                 output = model(input)
+                output = tta_model(input)
 
             if valid_labels is not None:
                 output = output[:, valid_labels]
@@ -262,9 +310,11 @@ def validate(args, checkpoint=None):
                 real_labels.add_result(output)
 
             # measure accuracy and record loss
-            pred = torch.sigmoid(output)
+#             pred = torch.sigmoid(output)
+            pred = output
             preds.append(pred.cpu().numpy())
             targets.append(target.cpu().numpy())
+            filenames.extend(filename)
 
             # measure elapsed time
             batch_time.update(time.time() - end)
@@ -282,58 +332,29 @@ def validate(args, checkpoint=None):
         preds = np.concatenate(preds)
         targets = np.concatenate(targets)
 
-    return preds, targets
-
+    return preds, targets, filenames
 
 def get_metric(predictions, targets):
     return bulk_multi_label_metrics(predictions, targets, 0.5)
 
 from sklearn.metrics import f1_score, precision_score, recall_score
+
 def main():
     setup_default_logging()
-    args = parser.parse_args()
+    args, _ = parser.parse_known_args()
 
     data_dir = args.data
-    
+
     preds_list = []
     args.data = os.path.join(data_dir, 'val')
-    for checkpoint in args.checkpoints.split(','):
-        data = torch.load(checkpoint, map_location='cpu')
-        
-        args.model = data['args'].model
-        args.amp   = data['args'].amp
-        args.dataset_type = data['args'].dataset_type
-        args.use_multi_label = data['args'].use_multi_label
-        args.num_classes = data['args'].num_classes
-        args.use_ema = data['args'].model_ema
-        
-        preds, targets = validate(args, data)
-        preds_list.append(preds)
-        
-    thresholds = calibrate(preds_list, targets)
+
+    preds, targets, filenames = validate(args, args.checkpoints.split(','))
+
+    thresholds = calibrate(preds, targets)
     print('thresholds: ', thresholds)
-    preds, predicts = ensemble(preds_list, targets, thresholds=thresholds)
-    f1 = f1_score(targets, predicts, average='macro')
-    print('calibrated_f1_in_val: ', f1)
-    metric = get_metric(preds, targets)
-    print('metric_in_val:', metric)
-    
-    preds_list = []
-    args.data = os.path.join(data_dir, 'test')
-    for checkpoint in args.checkpoints.split(','):
-        data = torch.load(checkpoint, map_location='cpu')
-        args.model = data['args'].model
-        args.amp   = data['args'].amp
-        args.dataset_type = data['args'].dataset_type
-        args.use_multi_label = data['args'].use_multi_label
-        args.num_classes = data['args'].num_classes
-        args.use_ema = data['args'].model_ema
-        
-        preds, targets = validate(args, data)
-        preds_list.append(preds)
-    
-    preds, predicts = ensemble(preds_list, targets, thresholds=thresholds)
-    
+
+    predicts = preds > thresholds
+
     f1 = f1_score(targets, predicts, average='macro')
     print('calibrated_f1(macro): ', f1)
     f1 = f1_score(targets, predicts, average=None)
@@ -342,6 +363,21 @@ def main():
     metric = get_metric(preds, targets)
     print(metric)
     
+    annotations = [
+        {
+            'id': idx,
+            'file_name': filename,
+            'object': [
+                {
+                    'box': [0, 0, 1, 1],
+                    'label': 'c{}'.format(label+1)
+                } for label in range(7) if predict[label]
+            ]
+        } for idx, (filename, predict) in enumerate(sorted(zip(filenames, predicts)))
+    ]
+    with open('/aichallenge/t3_res_caffeinism.json', 'w') as f:
+        f.write(json.dumps({'annotations': annotations}))
+
 def write_results(results_file, results):
     with open(results_file, mode='w') as cf:
         dw = csv.DictWriter(cf, fieldnames=results[0].keys())
